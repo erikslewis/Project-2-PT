c77ef6805ef6e3d9f2b84b2686a8b8aee82531c6
erikslewis <Adelina198527@gmail.com>
2017-01-03 15:00:57 -0500

    fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,7 +2,6 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-
 function perc1() {
 a = document.form1.a.value/100;
 b = a*document.form1.b.value;
@@ -17,7 +16,16 @@ d = c*100;
 document.form1.total2.value = d
                          };
 
+
+
+
+
 $(document).ready(function() {
+
+
+
+
+
 
 var timer = {
   seconds: 0,
@@ -62,6 +70,8 @@ var timer = {
 }
 
 timer.setupListeners();
+
+
 
 
 $(function(){
@@ -111,5 +121,62 @@ $(function(){
                         });
                       });
 
+                      var myNodelist = document.getElementsByTagName("LI");
+                      var i;
+                      for (i = 0; i < myNodelist.length; i++) {
+                        var span = document.createElement("SPAN");
+                        var txt = document.createTextNode("\u00D7");
+                        span.className = "close";
+                        span.appendChild(txt);
+                        myNodelist[i].appendChild(span);
+                      }
 
-     });
+                      // Click on a close button to hide the current list item
+                      var close = document.getElementsByClassName("close");
+                      var i;
+                      for (i = 0; i < close.length; i++) {
+                        close[i].onclick = function() {
+                          var div = this.parentElement;
+                          div.style.display = "none";
+                        }
+                      }
+
+                      // Add a "checked" symbol when clicking on a list item
+                      var list = document.querySelector('ul');
+                      list.addEventListener('click', function(ev) {
+                        if (ev.target.tagName === 'LI') {
+                          ev.target.classList.toggle('checked');
+                        }
+                      }, false);
+
+                      // Create a new list item when clicking on the "Add" button
+                      function newElement() {
+                        var li = document.createElement("li");
+                        var inputValue = document.getElementById("myInput").value;
+                        var t = document.createTextNode(inputValue);
+                        li.appendChild(t);
+                        if (inputValue === '') {
+                          alert("You must write something!");
+                        } else {
+                          document.getElementById("myUL").appendChild(li);
+                        }
+                        document.getElementById("myInput").value = "";
+
+                        var span = document.createElement("SPAN");
+                        var txt = document.createTextNode("\u00D7");
+                        span.className = "close";
+                        span.appendChild(txt);
+                        li.appendChild(span);
+
+                        for (i = 0; i < close.length; i++) {
+                          close[i].onclick = function() {
+                            var div = this.parentElement;
+                            div.style.display = "none";
+                          }
+                        }
+                      }
+                      $("#ton").on("click",function(){
+                        newElement();
+
+                      });
+                     });
