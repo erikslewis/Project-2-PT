70ecbe4a0033bcb0c85b85be3ed7ec668b42e0ed
erikslewis <Adelina198527@gmail.com>
2017-01-05 00:13:54 -0500

    workingmusic

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+//= require_tree
+
+
 function perc1() {
 a = document.form1.a.value/100;
 b = a*document.form1.b.value;
@@ -16,105 +18,91 @@ d = c*100;
 document.form1.total2.value = d
                          };
 
-                         var myVar;
-
-                         function myFunction() {
-                             myVar = setTimeout(showPage, 3000);
-                         }
-
-                         function showPage() {
-                           document.getElementById("loader").style.display = "none";
-                           document.getElementById("myDiv").style.display = "block";
-                         }
-
-                         (function () {
-                       var audio = new Audio();
-
-                       function searchTracks(query) {
-                           $.ajax({
-                               url: 'https://api.spotify.com/v1/search',
-                               data: {
-                                   q: query,
-                                   type: 'track'
-                               },
-                               success: function (response) {
-                                   if (response.tracks.items.length) {
-                                       var track = response.tracks.items[0];
-                                       audio.src = track.preview_url;
-                                       audio.play();
-                                       communicateAction('<div>Playing ' + track.name + ' by ' + track.artists[0].name + '</div><img width="150" src="' + track.album.images[1].url + '">');
-                                   }
-                               }
-                           });
-                       }
-
-                       function playSong(songName, artistName) {
-                           var query = songName;
-                           if (artistName) {
-                               query += ' artist:' + artistName;
-                           }
-
-                           searchTracks(query);
-                       }
-
-                       function communicateAction(text) {
-                           var rec = document.getElementById('conversation');
-                           rec.innerHTML += '<div class="action">' + text + '</div>';
-                       }
-
-                       function recognized(text) {
-                           var rec = document.getElementById('conversation');
-                           rec.innerHTML += '<div class="recognized"><div>' + text + '</div></div>';
-                       }
-
-                       if (annyang) {
-                           // Let's define our first command. First the text we expect, and then the function it should call
-                           var commands = {
-                               'stop': function () {
-                                   audio.pause();
-                               },
-                                   'play track *song': function (song) {
-                                   recognized('Play track ' + song);
-                                   playSong(song);
-                               },
-                                   'play *song by *artist': function (song, artist) {
-                                   recognized('Play song ' + song + ' by ' + artist);
-                                   playSong(song, artist);
-                               },
-                                   'play song *song': function (song) {
-                                   recognized('Play song ' + song);
-                                   playSong(song);
-                               },
-                                   'play *song': function (song) {
-                                   recognized('Play ' + song);
-                                   playSong(song);
-                               },
-
-                                   ':nomatch': function (message) {
-                                   recognized(message);
-                                   communicateAction('Sorry, I don\'t understand this action');
-                               }
-                           };
-
-                           // Add our commands to annyang
-                           annyang.addCommands(commands);
-
-                           // Start listening. You can call this here, or attach this call to an event, button, etc.
-                           annyang.start();
-                       }
-
-                       annyang.addCallback('error', function () {
-                           communicateAction('error');
-                       });
-                   })();
-
 
 
 $(document).ready(function() {
 
 
+  (function () {
+      var audio = new Audio();
 
+      function searchTracks(query) {
+          $.ajax({
+              url: 'https://api.spotify.com/v1/search',
+              data: {
+                  q: query,
+                  type: 'track'
+              },
+              success: function (response) {
+                  if (response.tracks.items.length) {
+                      var track = response.tracks.items[0];
+                      audio.src = track.preview_url;
+                      audio.play();
+                      communicateAction('<div>Playing ' + track.name + ' by ' + track.artists[0].name + '</div><img width="150" src="' + track.album.images[1].url + '">');
+                  }
+              }
+          });
+      }
 
+      function playSong(songName, artistName) {
+          var query = songName;
+          if (artistName) {
+              query += ' artist:' + artistName;
+          }
+
+          searchTracks(query);
+      }
+
+      function communicateAction(text) {
+          var rec = document.getElementById('conversation');
+          rec.innerHTML += '<div class="action">' + text + '</div>';
+      }
+
+      function recognized(text) {
+          var rec = document.getElementById('conversation');
+          rec.innerHTML += '<div class="recognized"><div>' + text + '</div></div>';
+      }
+
+      if (annyang) {
+          // Let's define our first command. First the text we expect, and then the function it should call
+          var commands = {
+              'stop': function () {
+                  audio.pause();
+              },
+                  'play track *song': function (song) {
+                  recognized('Play track ' + song);
+                  playSong(song);
+              },
+                  'play *song by *artist': function (song, artist) {
+                  recognized('Play song ' + song + ' by ' + artist);
+                  playSong(song, artist);
+              },
+                  'play song *song': function (song) {
+                  recognized('Play song ' + song);
+                  playSong(song);
+              },
+                  'play *song': function (song) {
+                  recognized('Play ' + song);
+                  playSong(song);
+              },
+
+                  ':nomatch': function (message) {
+                  recognized(message);
+                  communicateAction('Sorry, I don\'t understand this action');
+              }
+          };
+
+          // Add our commands to annyang
+          annyang.addCommands(commands);
+
+          // Start listening. You can call this here, or attach this call to an event, button, etc.
+          annyang.start();
+      }
+
+      annyang.addCallback('error', function () {
+          communicateAction('error');
+      });
+  })();
 
 
 var timer = {
@@ -538,4 +526,6 @@ $(function(){
             });
         }
           });
+
+
   });
