cd113761e60e4f23e105f52d39d55b7c916ea6ec
erikslewis <Adelina198527@gmail.com>
2017-01-05 13:27:57 -0500

    goood

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,8 +20,85 @@ document.form1.total2.value = d
 
 
 
-$(document).ready(function() {
 
+$(document).ready(function() {
+  var map,
+      currentPositionMarker,
+      mapCenter = new google.maps.LatLng(14.668626, 121.24295),
+      map;
+
+  // change the zoom if you want
+  function initializeMap()
+  {
+      map = new google.maps.Map(document.getElementById('map_canvas'), {
+         zoom: 18,
+         center: mapCenter,
+         mapTypeId: google.maps.MapTypeId.ROADMAP
+       });
+  }
+
+  function locError(error) {
+      // tell the user if the current position could not be located
+      alert("The current position could not be found!");
+  }
+
+  // current position of the user
+  function setCurrentPosition(pos) {
+      currentPositionMarker = new google.maps.Marker({
+          map: map,
+          position: new google.maps.LatLng(
+              pos.coords.latitude,
+              pos.coords.longitude
+          ),
+          title: "Current Position"
+      });
+      map.panTo(new google.maps.LatLng(
+              pos.coords.latitude,
+              pos.coords.longitude
+          ));
+  }
+
+  function displayAndWatch(position) {
+
+      // set current position
+      setCurrentPosition(position);
+
+      // watch position
+      watchCurrentPosition();
+  }
+
+  function watchCurrentPosition() {
+      var positionTimer = navigator.geolocation.watchPosition(
+          function (position) {
+              setMarkerPosition(
+                  currentPositionMarker,
+                  position
+              );
+          });
+  }
+
+  function setMarkerPosition(marker, position) {
+      marker.setPosition(
+          new google.maps.LatLng(
+              position.coords.latitude,
+              position.coords.longitude)
+      );
+  }
+
+  function initLocationProcedure() {
+      initializeMap();
+      if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(displayAndWatch, locError);
+      } else {
+          // tell the user if a browser doesn't support this amazing API
+          alert("Your browser does not support the Geolocation API!");
+      }
+  }
+
+  // initialize with a little help of jQuery
+  $(document).ready(function() {
+      initLocationProcedure();
+  });
 
   (function () {
       var audio = new Audio();
