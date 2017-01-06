d6f2f25e069c3a8218bc101baba12b652f95f29b
erikslewis <Adelina198527@gmail.com>
2017-01-06 14:33:15 -0500

    customgif

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -300,13 +300,7 @@ $(function(){
                         }
                       }
 
-                      // Add a "checked" symbol when clicking on a list item
-                      var list = document.querySelector('ul');
-                      list.addEventListener('click', function(ev) {
-                        if (ev.target.tagName === 'LI') {
-                          ev.target.classList.toggle('checked');
-                        }
-                      }, false);
+                      
 
                       // Create a new list item when clicking on the "Add" button
                       function newElement() {
